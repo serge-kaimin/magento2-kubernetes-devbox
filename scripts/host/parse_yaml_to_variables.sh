793373953787658267bc967d38e0afd777978413
Sergey Kaimin <serge.kaimin@gmail.com>
2020-01-03 10:36:17 +0300

    TODO

--- scripts/host/parse_yaml_to_variables.sh
+++ scripts/host/parse_yaml_to_variables.sh
@@ -2,20 +2,23 @@
 # @Arguments: $1 - source to import $2 - add prefix
 # reference: https://gist.github.com/pkuczynski/8665367
 
+#TODO comments
+#todo use - in variables
 parse_yaml() {
    local prefix=$2
-   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   local s='[[:space:]]*' 
+         w='[a-zA-Z0-9_]*' 
+         fs=$(echo @|tr @ '\034')
    sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
-        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
-   awk -F$fs '{
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  "$1" |
+   awk -F"$fs" '{
       indent = length($1)/2;
       vname[indent] = $2;
       for (i in vname) {if (i > indent) {delete vname[i]}}
       if (length($3) > 0) {
          vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
-         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+         printf("%s%s%s=\"%s\"\n", "export '$prefix'",vn, $2, $3);
       }
    }'
 }
 
-parse_yaml $1 $2
